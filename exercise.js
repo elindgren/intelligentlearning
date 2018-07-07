2d8fbb7070f7d55e88e9a74da80e1ae76a8ec5e0
Eric <ericlin@student.chalmers.se>
2018-07-07 15:57:14 +0200

    Created a first layout of exercises as well as some basic styling. Todo: Create an exercise page with input forms and create a more fancy exercies layout.

--- exercise.js
+++ exercise.js
@@ -2,14 +2,13 @@ window.onload = function sadboi() {
     for (i = 0; i < 10; i++) {
         //Create a button element and style it accordingly
         let element = document.createElement("button");
-        element.className="mdl-button mdl-js-button mdl-js-ripple-effect  exercise";
+        element.className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised exercise";
         element.id = "exercise" + i;
-        //element.style.backgroundImage = "url('images/hej.jpg')";
-        //element.style.backgroundSize = "250px 200px";
-        let t = document.createTextNode("Exercise " + i);
-        element.appendChild(t);
-        //Add an onclick-function which moves to the exercise page and loads the settings for the current exercise.
+        element.style.backgroundImage = "url('images/ex_uppgift.png')";
+        element.style.backgroundSize = "250px 200px";
+        element.innerText = "Exercise " + i;
         //element.innerHTML = '<img src="images/hej.jpg" width="10%" height = "10%"/>';
+        //Add an onclick-function which moves to the exercise page and loads the settings for the current exercise.
         element.onclick = function(){
             console.log(this.id);
             //Pass the current exercise as a parameter
