7f1d647af7baeafc22b42ddf1007f58aec19cbc0
Eric <ericlin@student.chalmers.se>
2018-07-08 17:23:19 +0200

    Added a question page with input form. Todo: Answer checking and marking of solved exercise.

--- exercise.js
+++ exercise.js
@@ -4,9 +4,18 @@ window.onload = function sadboi() {
         let element = document.createElement("button");
         element.className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised exercise";
         element.id = "exercise" + i;
-        element.style.backgroundImage = "url('images/ex_uppgift.png')";
-        element.style.backgroundSize = "250px 200px";
-        element.innerText = "Exercise " + i;
+        //Create a thumbnail and text element - place them inside the button.
+        let exerciseThumbnail = document.createElement("img");
+        exerciseThumbnail.src = "images/ex_uppgift.png";
+        exerciseThumbnail.style.height = "150px";
+        exerciseThumbnail.style.width = "125px";
+        // exerciseThumbnail.style.backgroundSize = "250px 200px";
+        let exerciseText = document.createTextNode("Exercise " + i);
+
+        element.appendChild(exerciseThumbnail);
+        element.appendChild(exerciseText);
+
+
         //element.innerHTML = '<img src="images/hej.jpg" width="10%" height = "10%"/>';
         //Add an onclick-function which moves to the exercise page and loads the settings for the current exercise.
         element.onclick = function(){
