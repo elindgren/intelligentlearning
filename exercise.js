f49c0d8d06ab8ff3a8349fabfba36e12ffbded0e
Eric <ericlin@student.chalmers.se>
2018-07-27 12:15:28 +0200

    Added a simple JSON database and functionality to read from the JSON file

--- exercise.js
+++ exercise.js
@@ -1,35 +1,40 @@
 window.onload = function sadboi() {
-    for (i = 0; i < 10; i++) {
+    let exerciseJSON = fetchExerciseInformation();
+    //Save the JSON locally for availability to other pages.
+    strinigifiedExerciseJSON = JSON.stringify(exerciseJSON);
+    localStorage.setItem("exerciseJSON", strinigifiedExerciseJSON);
+    console.log("JSON size:" + exerciseJSON.exercises.length);
+    for (i = 0; i < exerciseJSON.exercises.length; i++) {
         //Create a button element and style it accordingly
         let element = document.createElement("button");
-        element.className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised exercise";
-        element.id = "exercise_" + i;
+        element.className = "mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised exercise";
+        element.id = exerciseJSON.exercises[i].id;
         //Create a thumbnail and text element - place them inside the button.
         let exerciseThumbnail = document.createElement("img");
-        exerciseThumbnail.src = "images/ex_uppgift.png";
+        exerciseThumbnail.src = exerciseJSON.exercises[i].image_path;
         exerciseThumbnail.style.height = "150px";
         exerciseThumbnail.style.width = "125px";
         // exerciseThumbnail.style.backgroundSize = "250px 200px";
-        let exerciseText = document.createTextNode("Exercise " + i);
-
+        // let exerciseText = document.createTextNode(exerciseJSON.exercises[i].title);
         element.appendChild(exerciseThumbnail);
-        element.appendChild(exerciseText);
+        // element.appendChild(exerciseText);
 
 
         //element.innerHTML = '<img src="images/hej.jpg" width="10%" height = "10%"/>';
         //Add an onclick-function which moves to the exercise page and loads the settings for the current exercise.
-        element.onclick = function(){
+        element.onclick = function () {
             console.log(this.id);
             //Pass the current exercise as a parameter
-            location.href="exercise_page.html" + "?exercise=" + this.id;
+            location.href = "exercise_page.html" + "?exercise=" + this.id;
             return false;
         };
         exerciseContainer.appendChild(element);
     }
 };
+
 //This function returns to WeekOne. For breadcrumbs.
-function goToWeekOne(){
-    location.href="main.html"
+function goToWeekOne() {
+    location.href = "main.html"
 }
 
 function fetchExerciseInformation() {
@@ -40,6 +45,27 @@ function fetchExerciseInformation() {
     }, path);
     return actualJSON;
 }
+
+function fetchExerciseInformationByID(id) {
+    console.log("ID: " + id);
+    let temp = localStorage.getItem("exerciseJSON");
+    if (temp === null) {
+        temp = fetchExerciseInformation();
+        console.log("No JSON in localstorage");
+    } else {
+        temp = JSON.parse(temp);
+        console.log("JSON in localstorage");
+    }
+    for (i = 0; i < temp.exercises.length; i++) {
+        if (temp.exercises[i].id == id) {
+            console.log("Exercise found!");
+            return temp.exercises[i];
+        }
+    }
+    console.log("Exercise not found!");
+    return null;
+}
+
 /**
  * Loads JSON from path
  * @param callback, function that parses JSON
@@ -48,7 +74,7 @@ function fetchExerciseInformation() {
 function loadJSON(callback, path) {
     let xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open("GET", path, true);
+    xobj.open("GET", path, false);
     xobj.onreadystatechange = function () {
         if (xobj.readyState === 4 && xobj.status === 200) {
             callback(xobj.responseText);
